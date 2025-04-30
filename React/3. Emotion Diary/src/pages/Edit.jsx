d255b6e06aabd69167bd8f6208fa6076794332f6
iamjaeeuncho <redjoun@gmail.com>
2025-04-30 16:29:08 +0900

    react projects

---
+++ React/3. Emotion Diary/src/pages/Edit.jsx
@@ -0,0 +1,52 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
+import useDiary from "../hooks/useDiary";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import usePageTitle from "../hooks/usePageTitle";
+
+const Edit = () => {
+  const params = useParams();
+  const nav = useNavigate();
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+  const curDiaryItem = useDiary(params.id);
+  usePageTitle(`${params.id}번 일기 수정`);
+
+  const onclickDelete = () => {
+    if (window.confirm("일기를 정말 삭제하시겠습니까?")) {
+      onDelete(params.id);
+      nav("/", {
+        replace: true,
+      });
+    }
+  };
+
+  const onSubmit = (input) => {
+    if (window.confirm("일기를 정말 수정할까요?")) {
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={onclickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
+    </div>
+  );
+};
+
+export default Edit;
